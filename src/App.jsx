58529065ce3e5d0a2d41dc2b643446c528ff238b
Astha241105 <astha241105@gmail.com>
2024-10-20 11:55:48 +0530

    added update functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [title,setTitle]=useState('');
-  const [content,setcontent]=useState('');
-  const [notecount,setNotecount]=useState([])
-  const arr=[];
-  for (let i=0;i<localStorage.length;i++){
-    arr.push(localStorage.key(i));
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [notecount, setNotecount] = useState([]);
+
+  useEffect(() => {
+    const arr = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      arr.push(localStorage.key(i));
+    }
+    setNotecount(arr.map((key) => ({ t: key, c: localStorage.getItem(key) })));
+  }, []);
+
+  function saving() {
+    localStorage.setItem(title, content);
+    setNotecount([...notecount, { t: title, c: content }]);
+    console.log([...notecount, { t: title, c: content }]);
   }
-  function saving(){
-    localStorage.setItem(title,content);
+
+  function createNewNote() {
+    setTitle('');
+    setContent('');
   }
-  function createNewNote(){
-      setNotecount([...notecount,{t:title
-                                  ,c:content}]);
-      console.log(notecount);
-      setTitle('');
-      setcontent('');
-  } 
-   return (
+
+  function update(d) {
+    setTitle(d);
+    setContent(localStorage.getItem(d));
+    localStorage.removeItem(d);
+  }
+
+  return (
     <>
       <button onClick={createNewNote} id="adding">
         Add a new note
       </button>
-          <div id="container">
-          <input placeholder="Title"  value={title} onChange={(e)=>setTitle(e.target.value)}></input>
-          <textarea  rows="4" cols="8" value={content}  onChange={(e)=>setcontent(e.target.value) }/>
-          <button onClick={saving}>Save</button>
-          <ol>{
-            arr.map((data)=>
-            <li key={data} ><div>{key}</div><div>{localStorage.getItem(data)}</div></li>)}
-          </ol>
-          </div>
-        
+      <div id="container">
+        <input
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          rows="4"
+          cols="8"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button onClick={saving}>Save</button>
+        <ol>
+          {notecount.map((note) => (
+            <li key={note.t} onClick={() => update(note.t)}>
+              <div>{note.t}</div>
+              <div>{note.c}</div>
+            </li>
+          ))}
+        </ol>
+      </div>
     </>
   );
 }
