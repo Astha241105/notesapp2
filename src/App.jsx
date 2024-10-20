d1a83a5f75dda8d5ce9a036294e6b1c16f8f5663
Astha241105 <astha241105@gmail.com>
2024-10-20 11:00:19 +0530

    fetching notes list from local storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import './App.css';
 function App() {
   const [title,setTitle]=useState('');
   const [content,setcontent]=useState('');
+  const [notecount,setNotecount]=useState([])
+  const arr=[];
+  for (let i=0;i<localStorage.length;i++){
+    arr.push(localStorage.key(i));
+  }
   function saving(){
     localStorage.setItem(title,content);
   }
   function createNewNote(){
+      setNotecount([...notecount,{t:title
+                                  ,c:content}]);
+      console.log(notecount);
       setTitle('');
       setcontent('');
   } 
@@ -16,10 +24,14 @@ function App() {
       <button onClick={createNewNote} id="adding">
         Add a new note
       </button>
-          <div>
+          <div id="container">
           <input placeholder="Title"  value={title} onChange={(e)=>setTitle(e.target.value)}></input>
           <textarea  rows="4" cols="8" value={content}  onChange={(e)=>setcontent(e.target.value) }/>
           <button onClick={saving}>Save</button>
+          <ol>{
+            arr.map((data)=>
+            <li key={data} ><div>{key}</div><div>{localStorage.getItem(data)}</div></li>)}
+          </ol>
           </div>
         
     </>
